94802d4ae39012f2dc49398d64d909b62e68fada
eskokado <eskokado@gmail.com>
2023-03-01 05:35:42 -0300

    feat: generate files for call generate active_admin resource Requisicao

---
+++ app/admin/requisicoes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Requisicao do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :tipo, :data, :numero, :setor, :quantidade, :custo_medio, :custo_total, :observacoes, :estoque_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:tipo, :data, :numero, :setor, :quantidade, :custo_medio, :custo_total, :observacoes, :estoque_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end

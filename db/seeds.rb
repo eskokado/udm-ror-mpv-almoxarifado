64e10b3338419589be5cc92022e3e83b16fd5738
eskokado <eskokado@gmail.com>
2023-03-01 05:42:24 -0300

    feat: add Grupos into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,13 @@ Usuario.create!(email: 'colaborador@hotel.com',
                 status: 'Ativo',
                 password: '123456',
                 password_confirmation: '123456')
+
+cafe = Grupo.create! :nome => "Café", :status => "Ativo"
+acucar = Grupo.create! :nome => "Açúcar", :status => "Ativo"
+acucar = Grupo.create! :nome => "Açúcar", :status => "Ativo"
+cerveja = Grupo.create! :nome => "Cervejas", :status => "Ativo"
+limpeza = Grupo.create! :nome => "Material de Limpeza", :status => "Ativo"
+massa = Grupo.create! :nome => "Massas", :status => "Ativo"
+pao = Grupo.create! :nome => "Pão de Forma", :status => "Ativo"
+refrigerante = Grupo.create! :nome => "Refrigerantes", :status => "Ativo"
+suco = Grupo.create! :nome => "Sucos", :status => "Ativo"

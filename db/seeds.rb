7bbd6c68fbb89d8692810ee0b317ea59d7ab11fb
eskokado <eskokado@gmail.com>
2023-03-01 05:41:25 -0300

    feat: add Usuarios into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-Usuario.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Usuario.create!(email: 'administrador@hotel.com',
+                nome: 'Administrador',
+                perfil: 'Administrador',
+                status: 'Ativo',
+                password: '123456',
+                password_confirmation: '123456')
+
+Usuario.create!(email: 'gerente@hotel.com',
+                nome: 'Gerente',
+                perfil: 'Gerente',
+                status: 'Ativo',
+                password: '123456',
+                password_confirmation: '123456')
+
+Usuario.create!(email: 'colaborador@hotel.com',
+                nome: 'Colaborador',
+                perfil: 'Colaborador',
+                status: 'Ativo',
+                password: '123456',
+                password_confirmation: '123456')

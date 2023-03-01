bc733c4a7c80e5fa6c800963b16902c686a9d327
eskokado <eskokado@gmail.com>
2023-03-01 06:26:11 -0300

    feat: usuarios page with active admin

--- app/admin/usuarios.rb
+++ app/admin/usuarios.rb
@@ -1,28 +1,66 @@
 ActiveAdmin.register Usuario do
-  permit_params :email, :password, :password_confirmation
 
-  index do
+  menu priority: 6, label: "Usuários"
+
+  permit_params :nome, :perfil, :status, :email, :password, :password_confirmation
+
+  config.batch_actions = false
+
+  config.per_page = 10
+
+  config.sort_order = 'nome_asc'
+
+  filter :id, label: 'ID'
+  filter :nome, label: 'NOME'
+  filter :perfil, as: :select, label: 'PERFIL'
+  filter :email, label: 'EMAIL'
+  filter :status, as: :select, collection: Usuario.status, label: 'STATUS'
+
+  index title: "Usuários" do
+
     selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column "ID", :id
+    column "NOME", :nome
+    column "PERFIL", :perfil
+    column "EMAIL", :email
+    column "STATUS", :status
     actions
+
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  show title: :nome do
 
-  form do |f|
+    panel "Usuário" do
+
+      attributes_table_for usuario do
+
+        row("ID") { usuario.id }
+        row("NOME") { usuario.nome }
+        row("PERFIL") { usuario.perfil }
+        row("EMAIL") { usuario.email }
+        row("STATUS") { usuario.status }
+
+      end
+
+    end
+
+  end
+
+  form title: "Usuário" do |f|
+
     f.inputs do
+
+      f.input :nome
+      f.input :perfil
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :status
+
     end
+
     f.actions
+
   end
 
 end
